36f0acdf1d4c168b48f1d951236c0a6a6fc66fa0
Ernst Legaspi <legaspiernstdanielle@gmail.com>
2022-06-16 01:32:57 +0800

    added saved blogs gateways in backend

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken'
 const Auth = async (req, res, next) => {
 	try {
 		const token = req.headers.authorization.split(" ")[1]
-		let decodedData = jwt.verify(token, "test")
+		let decodedData = jwt.verify(token, "secret")
 		req.userId = decodedData?.id
 		next()
 	}
