60a021e579e931f3074dfec82f66d5dc3aa174be
Ernst Legaspi <legaspiernstdanielle@gmail.com>
2022-06-19 19:06:04 +0800

    added admin gateways in backend

--- backend/routes/blogRoute.js
+++ backend/routes/blogRoute.js
@@ -1,6 +1,7 @@
 import express from 'express'
 
 import Auth from '../middleware/auth.js'
+import AdminAuth from '../middleware/adminAuth.js'
 import { getBlogs, getSingleBlog, getLatestBlogs, getSavedBlogs, addSavedBlog, deleteSavedBlog, addBlog } from '../controllers/blogsController.js'
 
 const route = express.Router()
@@ -8,7 +9,7 @@ const route = express.Router()
 route.get(`/`, getBlogs)
 route.get(`/id/:title`, getSingleBlog)
 route.get(`/latest`, getLatestBlogs)
-route.post(`/`, Auth, addBlog)
+route.post(`/`, AdminAuth, addBlog)
 route.get(`/saved`, Auth, getSavedBlogs)
 route.post(`/saved`, Auth, addSavedBlog)
 route.delete(`/saved/:id`, Auth, deleteSavedBlog)
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,11 @@
 import express from 'express'
 
-import { loginController, registerController } from '../controllers/userController.js'
+import { loginController, registerController, adminLoginController } from '../controllers/userController.js'
 
 const route = express.Router()
 
 route.post(`/login`, loginController)
 route.post(`/register`, registerController)
+route.post(`/admin-login`, adminLoginController)
 
 export default route
